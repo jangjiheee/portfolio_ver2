3f39f2fd4c2bf6e5721e82e1d8f8e19e3a355bc4
jangjiheee <jihee.jang210@gmail.com>
2023-01-25 16:21:57 +0900

    dark mode 컬러 선택

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
 window.onload = function() {
     const toggle = document.getElementById("__mode_btn");
     const header = document.querySelector('header');
-    const section = document.querySelectorAll('section')
+    const footer = document.querySelector('footer');
     console.log(header);
+    const section = document.querySelectorAll('section');
+    const dot = document.querySelectorAll('.__double_dot');
+    console.log(dot);
+    // const footer = document.querySelector('footer');
     toggle.onclick = function() {
         toggle.classList.toggle('active');
         header.classList.toggle('change');
-        item.forEach(item => {
-            section.classList.toggle('change');
-        });
+        footer.classList.toggle('change');
+        for(i = 0; i<section.length; i++) {
+            section[i].classList.toggle('change');
+        }
+        for(j = 0; j<dot.length; j++) {
+            dot[j].classList.toggle('change');
+        }
+        // item.document.documentElement.style.setProperty('background', 'black');
     }
 }
