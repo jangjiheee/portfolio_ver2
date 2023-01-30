21ee50c0b9b500193cc0d321c637a61b93877722
jangjiheee <jihee.jang210@gmail.com>
2023-01-30 11:00:47 +0900

    반응형 에러 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,11 @@ window.onload = function() {
     const section = document.querySelectorAll('section');
     const dot = document.querySelectorAll('.__double_dot');
     console.log(dot);
+    const goSite = document.querySelectorAll(".__site");
+    const url = document.querySelectorAll(".__go_site > a");
+    const pagination = document. querySelectorAll("span.swiper-pagination-bullet");
+    console.log(pagination);
+
     // const footer = document.querySelector('footer');
     toggle.onclick = function() {
         toggle.classList.toggle('active');
@@ -17,6 +22,14 @@ window.onload = function() {
         for(j = 0; j<dot.length; j++) {
             dot[j].classList.toggle('change');
         }
+        for(k=0; k<goSite.length; k++) {
+            goSite[k].classList.toggle("active");
+            url[k].classList.toggle("active");
+            // underLine[k].classList.toggle(".active");
+        }
+        for (i = 0; i < pagination.length; i++) {
+            pagination[i].classList.toggle("change");
+        } 
         // item.document.documentElement.style.setProperty('background', 'black');
     }
 }
