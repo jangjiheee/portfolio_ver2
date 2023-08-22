ab519c0e1fc3b81fb3013a4f8a9ae49e837d7380
jangjiheee <jihee.jang210@gmail.com>
2023-08-22 22:53:56 +0900

    version2 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     const toggle = document.getElementById("__mode_btn");
+    console.log(toggle);
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
     console.log(header);
@@ -11,6 +12,7 @@ window.onload = function() {
     const pagination = document. querySelectorAll("span.swiper-pagination-bullet");
     console.log(pagination);
 
+
     const mobileBtn = document.querySelector(".__mobile_btn");
     const line = document.querySelectorAll(".line");
     const moNav = document.querySelector(".mobile_nav")
@@ -19,5 +21,10 @@ window.onload = function() {
             line[i].classList.toggle("active");
         }
         moNav.classList.toggle("active");
+    })
+
+
+    toggle.addEventListener("click", function() {
+        this.classList.toggle("active");
     })
 }
